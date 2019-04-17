1a203bf1ecf1b1931cbfecd8f465b01a2b99628c
jpcv222 <juan.castro.vasquez@correounivalle.edu.co>
2019-04-16 19:34:42 -0500

    Comment unused files

--- UnivalleThemePlugin.php
+++ UnivalleThemePlugin.php
@@ -38,13 +38,13 @@ class UnivalleThemePlugin extends ThemePlugin {
 
 		
 		// Load Bootsrap's dropdown
-		$this->addScript('popper', 'js/lib/popper/popper.js');
-		$this->addScript('bsUtil', 'js/lib/bootstrap/util.js');
-		$this->addScript('bsDropdown', 'js/lib/bootstrap/dropdown.js');
+		//$this->addScript('popper', 'js/lib/popper/popper.js');
+		//$this->addScript('bsUtil', 'js/lib/bootstrap/util.js');
+		//$this->addScript('bsDropdown', 'js/lib/bootstrap/dropdown.js');
 		$this->addScript('bsJs', 'bootstrap/js/bootstrap.min.js');
 
 		// Load custom JavaScript for this theme
-		$this->addScript('default', 'js/main.js');
+		//$this->addScript('default', 'js/main.js');
 
 	}
 
